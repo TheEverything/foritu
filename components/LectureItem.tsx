c948f1361e5cbe5980112a015382037626df8d6d
Yiğit Tilaver <yigittilaver2000@gmail.com>
2024-01-11 20:53:25 +0300

    developed codes with grade

--- components/LectureItem.tsx
+++ components/LectureItem.tsx
@@ -1,11 +1,24 @@
+import db from "@/lib/db";
 import { cn } from "@/lib/utils";
+import { Lecture } from "@prisma/client";
 
 interface Props {
   className?: string;
+  lecture: Lecture;
 }
 
-function LectureItem({ className }: Props) {
-  return <div className={cn("w-[]379px", className)}></div>;
+async function LectureItem({ className, lecture }: Props) {
+  const gradesById = await db.grade.findMany({ where: { lectureId: { equals: lecture.id } } });
+  return (
+    <div className={cn("w-[379px] h-fit", className)}>
+      <div className="w-fit h-fit flex flex-row justify-center items-center gap-2 p-2 overflow-hidden">{lecture.title}</div>
+      <div className="w-fit h-fit flex flex-row justify-center items-center gap-2 p-2 overflow-hidden">
+        {gradesById.map((grade) => (
+          <div className="w-8 h-8 flex flex-row justify-center items-center gap-2 p-2 overflow-hidden">{grade.note}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default LectureItem;
